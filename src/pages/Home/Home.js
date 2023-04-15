98653490f035089c7c1fdb97e22b2f6d8cef433c
Sithu <cithukyaw@gmail.com>
2023-04-15 19:46:39 +0630

    Add mui and design home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,18 @@
+import {Button} from "@mui/material";
+import LoginIcon from '@mui/icons-material/Login';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import './Home.scss';
+
 function Home() {
     return (
-        <div>Home</div>
+        <>
+            <h1 className="text-center">Help Mommy</h1>
+            <div className="block-img">
+                <img src="/img/undraw_family.svg" alt="Help Mommy" className="img-fluid" />
+            </div>
+            <Button variant="contained" size="large" fullWidth="true" href="/login" startIcon={<LoginIcon/>}>Login</Button>
+            <Button variant="outlined" size="large" fullWidth="true" href="/signup" startIcon={<HowToRegIcon/>}>Signup</Button>
+        </>
     );
 }
 

d0fbc766d82f0a31c33255858eb8e72ae5d27d23
Sithu <cithukyaw@gmail.com>
2023-04-15 18:27:15 +0630

    Add route and pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import './App.scss';
+import "./App.scss";
+import {
+    createBrowserRouter,
+    Outlet,
+    RouterProvider
+} from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import Home from "./pages/Home/Home";
+
+const Layout = () => {
+    return (
+        <div className="app">
+            <Outlet/>
+        </div>
+    );
+};
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+            },
+            {
+                path: "/login",
+                element: <Login/>
+            },
+            {
+                path: "/signup",
+                element: <Signup/>
+            },
+        ]
+    },
+]);
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                Help Mommy
-            </header>
+        <div>
+            <RouterProvider router={router}/>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const renderReactDom = () => {
     const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -15,7 +15,7 @@ const renderReactDom = () => {
 };
 
 if (window.cordova) {
-    document.addEventListener('deviceready', () => {
+    document.addEventListener("deviceready", () => {
         renderReactDom();
     }, false);
 } else {

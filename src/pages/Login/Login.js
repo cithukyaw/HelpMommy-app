49ab1dfe082604411b20c91abcaa14d24d03a5b3
Sithu <cithukyaw@gmail.com>
2023-04-17 16:57:54 +0630

    Add login and signup page form designs

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,27 @@
+import {Button, TextField} from "@mui/material";
+import LoginIcon from '@mui/icons-material/Login';
+import {Link} from "react-router-dom";
 
 function Login() {
     return (
-        <div>Login</div>
+        <div>
+            <div className="text-center">
+                <LoginIcon fontSize="large" className="rounded-icon icon-primary"/>
+            </div>
+            <h2 className="text-center">Login</h2>
+            <div className="form-control">
+                <TextField id="outlined-basic" label="Username" variant="outlined" required fullWidth />
+            </div>
+            <div className="form-control">
+                <TextField id="outlined-basic" label="Password" variant="outlined" required fullWidth type="password" />
+            </div>
+            <div className="form-control">
+                <Button variant="contained" size="large" fullWidth={true}>Login</Button>
+            </div>
+            <div className="text-center">
+                <Link to="/signup">If you don&apos;t have an account, create an account here</Link>
+            </div>
+        </div>
     );
 }
 
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,7 +1,27 @@
+import {Button, TextField} from "@mui/material";
+import HowToRegIcon from "@mui/icons-material/HowToReg";
+import {Link} from "react-router-dom";
 
 function Signup() {
     return (
-        <div>Signup</div>
+        <div>
+            <div className="text-center">
+                <HowToRegIcon fontSize="large" className="rounded-icon icon-primary"/>
+            </div>
+            <h2 className="text-center">Create an Account</h2>
+            <div className="form-control">
+                <TextField id="outlined-basic" label="Username" variant="outlined" required fullWidth />
+            </div>
+            <div className="form-control">
+                <TextField id="outlined-basic" label="Password" variant="outlined" required fullWidth type="password" />
+            </div>
+            <div className="form-control">
+                <Button variant="contained" size="large" fullWidth={true}>Create</Button>
+            </div>
+            <div className="text-center">
+                <Link to="/login">If you already have an account, login here</Link>
+            </div>
+        </div>
     );
 }
 

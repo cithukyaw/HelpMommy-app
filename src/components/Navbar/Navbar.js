05ef8538a8fdfdc4dee9ddaa8628da32c2856b00
Sithu <cithukyaw@gmail.com>
2023-04-23 21:35:53 +0630

    Remove deprecated activeClassName usage

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,24 +7,26 @@ import {NavLink} from "react-router-dom";
 import "./Navbar.scss";
 
 const Navbar = () => {
+    const menuActive = (navData) => navData.isActive ? "active" : '';
+
     return (
         <nav>
-            <NavLink to="/dashboard" activeClassName="active">
+            <NavLink to="/dashboard" className={menuActive}>
                 <HomeIcon />
                 <div>Home</div>
             </NavLink>
-            <NavLink to="/hearts" activeClassName="active">
+            <NavLink to="/hearts" className={menuActive}>
                 <InterestsIcon />
                 <div>Hearts</div>
             </NavLink>
-            <NavLink to="/add" activeClassName="active">
+            <NavLink to="/add" className={menuActive}>
                 <AddCircleOutlineIcon className="ico-add" />
             </NavLink>
-            <NavLink to="/exchange" activeClassName="active">
+            <NavLink to="/exchange" className={menuActive}>
                 <CurrencyExchangeIcon />
                 <div>Exchange</div>
             </NavLink>
-            <NavLink to="/account" activeClassName="active">
+            <NavLink to="/account" className={menuActive}>
                 <AccountCircleIcon />
                 <div>Account</div>
             </NavLink>

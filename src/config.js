d3b59cf163742b77944ddfa3ced2781cc45fdc63
Sithu <cithukyaw@gmail.com>
2023-04-30 17:03:13 +0630

    Display heart balance in history list

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    "exchangeRate": 100, // MMK per 1 heart
+    "currencyUnit": "Ks",
+};
+
+export default config;
--- src/pages/Hearts/Hearts.js
+++ src/pages/Hearts/Hearts.js
@@ -1,13 +1,29 @@
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
 import ListCard from "../../components/ListCard/ListCard";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import config from "../../config";
+import {Card, CardContent, Typography} from "@mui/material";
 
 const Hearts = () => {
+    const totalHearts = 30;
+    const amount = totalHearts * config.exchangeRate;
+
     return (
         <>
             <Header title="Hearts Earned History"/>
             <div className="container">
-                <ListCard title="Today" index={1}/>
+                <Card>
+                    <CardContent>
+                        <Typography variant="h6" component="div">
+                            You have total {totalHearts}<FavoriteIcon/>
+                        </Typography>
+                        <Typography variant="body1">
+                            worth of <strong>{amount.toLocaleString()} {config.currencyUnit}</strong>
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <ListCard title="Today"/>
                 <ListCard title="Yesterday"/>
                 <ListCard title="2 days ago"/>
             </div>

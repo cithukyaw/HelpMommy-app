ae4fb4620c112a30a58d325153894cb333b2cb13
Sithu <cithukyaw@gmail.com>
2023-05-18 22:11:32 +0630

    Add visibility icon to password field

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,17 +1,26 @@
 import {useForm} from "react-hook-form";
-import {Button, TextField} from "@mui/material";
-import LoginIcon from "@mui/icons-material/Login";
+import {useState} from "react";
 import {Link, useNavigate} from "react-router-dom";
+import {Button, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField} from "@mui/material";
+import FormControl from '@mui/material/FormControl';
+import LoginIcon from "@mui/icons-material/Login";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
 import Error from "../../components/Error";
 
 const Login = () => {
     const navigate = useNavigate();
-
+    const [showPassword, setShowPassword] = useState(false);
     const {
         register,
         formState: {errors},
         handleSubmit
     } = useForm();
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
 
     const onSubmit = () => {
         navigate("/dashboard");
@@ -26,12 +35,30 @@ const Login = () => {
             <div className="form-control">
                 <Error field={errors.username} />
                 <TextField {...register("username", {required: "Enter username."})}
-                    id="outlined-basic" label="Username" variant="outlined" required fullWidth />
+                    id="outlined-username" label="Username" variant="outlined" required fullWidth />
             </div>
             <div className="form-control">
                 <Error field={errors.password} />
-                <TextField {...register("password", {required: "Enter password."})}
-                    id="outlined-basic" label="Password" variant="outlined" required fullWidth type="password" />
+                <FormControl variant="outlined" fullWidth>
+                    <InputLabel htmlFor="outlined-password">Password</InputLabel>
+                    <OutlinedInput
+                        {...register("password", {required: "Enter a password with min 8 characters."})}
+                        id="outlined-password" label="Password" required
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
             </div>
             <div className="form-control">
                 <Button onClick={handleSubmit(onSubmit)}
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,19 +1,27 @@
 import {useForm} from "react-hook-form";
-import {Button, TextField} from "@mui/material";
+import {useState} from "react";
+import {Button, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField} from "@mui/material";
+import FormControl from '@mui/material/FormControl';
 import HowToRegIcon from "@mui/icons-material/HowToReg";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
 import {Link, useNavigate} from "react-router-dom";
 import Error from "../../components/Error";
 
 // eslint-disable-next-line
 const Signup = () => {
     const navigate = useNavigate();
-
+    const [showPassword, setShowPassword] = useState(false);
     const {
         register,
         formState: {errors},
         handleSubmit
     } = useForm();
 
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
     const onSubmit = () => {
         navigate("/login");
     };
@@ -27,12 +35,30 @@ const Signup = () => {
             <div className="form-control">
                 <Error field={errors.username} />
                 <TextField {...register("username", {required: "Enter a username."})}
-                    id="outlined-basic" label="Username" variant="outlined" required fullWidth autoComplete="off" />
+                    id="outlined-username" label="Username" variant="outlined" required fullWidth autoComplete="off" />
             </div>
             <div className="form-control">
                 <Error field={errors.password} />
-                <TextField {...register("password", {required: "Enter a password with min 8 characters."})}
-                    id="outlined-basic" label="Password" variant="outlined" required fullWidth type="password" />
+                <FormControl variant="outlined" fullWidth>
+                    <InputLabel htmlFor="outlined-password">Password</InputLabel>
+                    <OutlinedInput
+                        {...register("password", {required: "Enter a password with min 8 characters."})}
+                        id="outlined-password" label="Password" required
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                           <InputAdornment position="end">
+                               <IconButton
+                                   aria-label="toggle password visibility"
+                                   onClick={handleClickShowPassword}
+                                   onMouseDown={handleMouseDownPassword}
+                                   edge="end"
+                               >
+                                   {showPassword ? <VisibilityOff /> : <Visibility />}
+                               </IconButton>
+                           </InputAdornment>
+                        }
+                    />
+                </FormControl>
             </div>
             <div className="form-control">
                 <Button onClick={handleSubmit(onSubmit)}

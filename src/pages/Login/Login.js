bd34f637b8678feda323500754859e9b59c5d816
Sithu <cithukyaw@gmail.com>
2023-05-18 21:53:16 +0630

    Add form validation to login and signup forms

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,19 @@
+import {useForm} from "react-hook-form";
 import {Button, TextField} from "@mui/material";
 import LoginIcon from "@mui/icons-material/Login";
 import {Link, useNavigate} from "react-router-dom";
+import Error from "../../components/Error";
 
 const Login = () => {
     const navigate = useNavigate();
 
-    const handleClick = () => {
+    const {
+        register,
+        formState: {errors},
+        handleSubmit
+    } = useForm();
+
+    const onSubmit = () => {
         navigate("/dashboard");
     };
 
@@ -16,13 +24,18 @@ const Login = () => {
             </div>
             <h2 className="text-center">Login</h2>
             <div className="form-control">
-                <TextField id="outlined-basic" label="Username" variant="outlined" required fullWidth />
+                <Error field={errors.username} />
+                <TextField {...register("username", {required: "Enter username."})}
+                    id="outlined-basic" label="Username" variant="outlined" required fullWidth />
             </div>
             <div className="form-control">
-                <TextField id="outlined-basic" label="Password" variant="outlined" required fullWidth type="password" />
+                <Error field={errors.password} />
+                <TextField {...register("password", {required: "Enter password."})}
+                    id="outlined-basic" label="Password" variant="outlined" required fullWidth type="password" />
             </div>
             <div className="form-control">
-                <Button className="margin-button" variant="contained" size="large" fullWidth={true} onClick={handleClick}>Login</Button>
+                <Button onClick={handleSubmit(onSubmit)}
+                    className="margin-button" variant="contained" size="large" fullWidth={true}>Login</Button>
             </div>
             <div className="text-center">
                 <Link to="/signup">If you don&apos;t have an account, create an account here</Link>
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,9 +1,23 @@
+import {useForm} from "react-hook-form";
 import {Button, TextField} from "@mui/material";
 import HowToRegIcon from "@mui/icons-material/HowToReg";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import Error from "../../components/Error";
 
 // eslint-disable-next-line
 const Signup = () => {
+    const navigate = useNavigate();
+
+    const {
+        register,
+        formState: {errors},
+        handleSubmit
+    } = useForm();
+
+    const onSubmit = () => {
+        navigate("/login");
+    };
+
     return (
         <div className="container">
             <div className="text-center">
@@ -11,13 +25,18 @@ const Signup = () => {
             </div>
             <h2 className="text-center">Create an Account</h2>
             <div className="form-control">
-                <TextField id="outlined-basic" label="Username" variant="outlined" required fullWidth />
+                <Error field={errors.username} />
+                <TextField {...register("username", {required: "Enter a username."})}
+                    id="outlined-basic" label="Username" variant="outlined" required fullWidth autoComplete="off" />
             </div>
             <div className="form-control">
-                <TextField id="outlined-basic" label="Password" variant="outlined" required fullWidth type="password" />
+                <Error field={errors.password} />
+                <TextField {...register("password", {required: "Enter a password with min 8 characters."})}
+                    id="outlined-basic" label="Password" variant="outlined" required fullWidth type="password" />
             </div>
             <div className="form-control">
-                <Button className="margin-button" variant="contained" size="large" fullWidth={true}>Create</Button>
+                <Button onClick={handleSubmit(onSubmit)}
+                    className="margin-button" variant="contained" size="large" fullWidth={true}>Create</Button>
             </div>
             <div className="text-center">
                 <Link to="/login">If you already have an account, login here</Link>

72f996ff412d96672f109a723794dfabb7a8cc4c
Sithu <cithukyaw@gmail.com>
2023-06-04 11:08:52 +0630

    Remove StrictMode to prevent useEffect run twice

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
@@ -8,9 +7,7 @@ const renderReactDom = () => {
     const root = ReactDOM.createRoot(document.getElementById("root"));
 
     root.render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
+        <App />
     );
 };
 

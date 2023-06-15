2c4356ccf234fd5e1f72609782c7adfa8c670e7b
Sithu <cithukyaw@gmail.com>
2023-06-15 22:32:49 +0630

    Update dashboard using API for jobs render

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -8,18 +8,28 @@ import {Link} from "react-router-dom";
 import "./Dashboard.scss";
 import {getItem} from "../../helpers/storage";
 import config from "../../config";
+import useFetch from "../../hooks/useFetch";
+import Loading from "../../components/Loading";
 
 const Dashboard = () => {
     const user = getItem(config.userStoreKey);
-    const name = user.full_name;
     const todayHearts = 12;
+
+    const getCurrentDate = () => {
+        const current = new Date();
+        return `${current.getFullYear()}-${current.getMonth() + 1}-${current.getDate()}`;
+    };
+
+    const { result, loading } = useFetch(`users/${user.id}/jobs?filter[date]=${getCurrentDate()}`);
+    const jobs = result?.data;
 
     return (
         <>
+            {loading && <Loading/>}
             <Header title="HelpMommy"/>
             <div className="container">
                 <div className="card">
-                    <div className="user-name">Welcome {name}</div>
+                    <div className="user-name">Welcome {user.full_name}</div>
                     <div className="user-hearts">
                         <FavoriteIcon/>
                         <strong>0 hearts</strong>
@@ -28,39 +38,28 @@ const Dashboard = () => {
                         Add Hearts
                     </Button>
                 </div>
-                <div className="text-center heart-broken">
-                    <HeartBrokenIcon/>
-                    <p>No hearts today</p>
-                    <p>Help your mom and get hearts.</p>
-                    <Button variant="contained" className="margin-button" component={Link} to="/add" startIcon={<AddCircleOutlineIcon/>}>
-                        Add Hearts
-                    </Button>
-                </div>
-                <div className="card">
-                    <h4 className="margin-top-none">{todayHearts} hearts earned today</h4>
-                    <ul className="list">
-                        <li>
-                            <span>အဝတ်လျော်</span>
-                            <span>5 <FavoriteIcon/></span>
-                        </li>
-                        <li>
-                            <span>စာလုပ်</span>
-                            <span>5 <FavoriteIcon/></span>
-                        </li>
-                        <li>
-                            <span>တံမြတ်စည်းလှည်း</span>
-                            <span>3 <FavoriteIcon/></span>
-                        </li>
-                        <li>
-                            <span>ဂိမ်းဆော့</span>
-                            <span>-2 <HeartBrokenIcon/></span>
-                        </li>
-                        <li>
-                            <span>YouTube ကြည့်</span>
-                            <span>-1 <HeartBrokenIcon/></span>
-                        </li>
-                    </ul>
-                </div>
+                { jobs && result.meta.total ?
+                    <div className="card">
+                        <h4 className="margin-top-none">{todayHearts} hearts earned today</h4>
+                        <ul className="list">
+                            {jobs.map(job => (
+                                <li key={job.id}>
+                                    <span>{job.name}</span>
+                                    <span>{job.rating} <FavoriteIcon/></span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    :
+                    <div className="text-center heart-broken">
+                        <HeartBrokenIcon/>
+                        <p>No hearts today</p>
+                        <p>Help your mom and get hearts.</p>
+                        <Button variant="contained" className="margin-button" component={Link} to="/add" startIcon={<AddCircleOutlineIcon/>}>
+                            Add Hearts
+                        </Button>
+                    </div>
+                }
             </div>
             <Navbar/>
         </>
